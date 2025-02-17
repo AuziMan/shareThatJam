fa12a8844e0a0fc058582711236c9e7ba15f5e35
AuziMan <austintdriver@gmail.com>
2025-02-17 12:32:07 -0700

    added track search

--- server/spotify.py
+++ server/spotify.py
@@ -6,9 +6,10 @@ from server.utils import get_access_token
 
 songs_blueprint = Blueprint("songs", __name__)
 
+BASE_SPOTIFY_URL = "https://api.spotify.com/v1"
+SPOTIFY_TOP_TRACKS_ENDPOINT = f"{BASE_SPOTIFY_URL}/me/top/tracks"
+SPOTIFY_NOW_PLAYING_ENDPOINT = f"{BASE_SPOTIFY_URL}/me/player/currently-playing"
 
-SPOTIFY_TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks"
-SPOTIFY_NOW_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing"
 
 
 
@@ -38,5 +39,22 @@ def get_now_playing():
             return jsonify(response.json())
         else:
             return jsonify({"error": f"Failed to fetch now playing: {response.status_code}"}), response.status_code
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+@songs_blueprint.route('/track/<string:trackId>', methods=['GET'])
+def get_track_by_id(trackId):
+    try:
+        access_token = get_access_token()
+        headers = {"Authorization": f"Bearer {access_token}"}
+        endpoint = f"{BASE_SPOTIFY_URL}/tracks/{trackId}"
+        print(endpoint)
+        response = requests.get(endpoint, headers=headers)
+
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({"error": f"Failed to fetch track by trackId: {trackId}, {response.status_code}"}), response.status_code
     except Exception as e:
         return jsonify({"error": str(e)}), 500
